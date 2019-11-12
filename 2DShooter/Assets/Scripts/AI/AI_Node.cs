c3845a8181bf0db40a1dee3960d573d7b5f130a8
Timothy Coish <tdcoish@gmail.com>
2019-11-12 11:05:51 -0800

    Editor Hacks and connecting nodes
    Nodes connected and this process made easier through shortcuts.

---
+++ 2DShooter/Assets/Scripts/AI/AI_Node.cs
@@ -0,0 +1,19 @@
+﻿/************************************************************
+************************************************************/
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AI_Node : MonoBehaviour
+{
+    public List<AI_Node>                        rConNodes;
+
+    void OnDrawGizmos ()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
+
+        foreach(var node in rConNodes){
+            Gizmos.DrawLine(transform.position, node.transform.position);
+        }
+        
+    }
+}
