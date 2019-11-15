25339bcd820d27cd33343dfe196438e163ef59fb
Timothy Coish <tdcoish@gmail.com>
2019-11-15 14:24:42 -0800

    Playground 3

---
+++ 2DShooter/Assets/Scripts/Level/LVL_Man.cs
@@ -0,0 +1,20 @@
+﻿/************************************************************
+Manages spawns, listens for enemies dying, etcetera.
+************************************************************/
+using UnityEngine;
+
+public class LVL_Man : MonoBehaviour
+{
+    public LVL_Spawner[]                            rSpawners;
+    public EN_Melee                                 PF_Melee;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
